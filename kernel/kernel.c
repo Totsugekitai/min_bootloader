47107f1d2bc2d8775e1998019fa625161dc7eb27
totsugekitai <hrstmyk811m@gmail.com>
2020-01-23 14:20:11 +0900

    fix

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,10 +9,9 @@ void entry_point(bootinfo_t *binfo)
     uint32_t ppsl = binfo->vinfo.ppsl;
     pix_format *fb = (pix_format *)binfo->vinfo.fb;
 
-    pix_format bgr = {0x66, 0xff, 0x99, 0x00};
-
     for (uint32_t i = 0; i < y_axis; i++) {
         for (uint32_t j = 0; j < x_axis; j++) {
+            pix_format bgr = {i % 0xff, j % 0xff, (i*j) % 0xff, 0x00};
             fb[i * ppsl + j] = bgr;
         }
     }
